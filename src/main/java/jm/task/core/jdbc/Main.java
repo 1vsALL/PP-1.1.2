adc8057ac44a5ab78a6bcf5808354a1f1420f6a7
1vsALL <alikkv@mail.ru>
2024-04-19 09:50:45 +0300

    pp
    commit

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,24 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.dropUsersTable();
+        userService.createUsersTable();
+
+        userService.saveUser("One", "lastOne", (byte) 1);
+        userService.saveUser("Second", "lastSecond", (byte) 2);
+        userService.saveUser("Third", "lastThird", (byte) 3);
+        userService.saveUser("Fourth", "lastFourth", (byte) 4);
+
+        userService.getAllUsers().stream().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+    }
+}
